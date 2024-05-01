678ed8d636625dcca1b21f6e2b369e66f1ca8ea2
AsmaaZaoud <99315884+AsmaaZaoud@users.noreply.github.com>
2024-05-01 20:15:15 +0300

    navigation

--- Rahma/App.js
+++ Rahma/App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'; // Assuming you're using Expo for the project
+import { createStackNavigator } from '@react-navigation/stack';
+import { Icon } from 'react-native-elements';
 
-export default function App() {
+// import Icon from 'react-native-vector-icons/FontAwesome';
+
+function Feed() {
   return (
-    <View style={styles.container}>
-      <Text>asma up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed hehdhfhfhfh</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Article() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Article Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Feed" component={BottomTabs}  />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
+function BottomTabs() {
+  return (
+    <Tab.Navigator
+      
+    >
+      <Tab.Screen name="Feed" component={Feed}  />
+      <Tab.Screen name="Article" component={Article} options={{ headerShown: false }}  />
+    </Tab.Navigator>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Tabs" component={MyDrawer} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+}
